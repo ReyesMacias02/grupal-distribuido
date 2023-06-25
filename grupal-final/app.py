18c84d766d0937b8dcdf9bbf8329cba6030f206d
jose reyes <jjrm1351828189@gmail.com>
2023-06-25 03:50:06 -0500

    toast en egreso

--- grupal-final/app.py
+++ grupal-final/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for,session
 from pymongo import MongoClient
+from datetime import datetime
 from bson import ObjectId
 app = Flask(__name__)
 client = MongoClient('mongodb://administrador:asd123@localhost:27017/?authMechanism=DEFAULT&authSource=admin')
@@ -35,14 +36,109 @@ def login():
 def cajero():
     return render_template('cajero.html')
 
-@app.route('/egreso')
+@app.route('/egreso', methods=['GET'])
 def egreso():
-    return render_template('egreso.html')
+    # Lógica para verificar la cédula y autenticar al usuario
+    usernid = session.get('username')
+    usuario_id = ObjectId(usernid)
+    collection = db['clientes']
+    print(usuario_id)
+    result = collection.find({"_id": usuario_id})
+    result=list(result)
+    for doc in result:
+        mount = doc.get("monto")
+    print(mount)
+    monto=mount
+    if 'resultado_egreso' in session:
+        message = session.get('resultado_egreso')
+        session.pop('resultado_egreso')
+    else:
+        message = "null"
+    return render_template('egreso.html',monto=monto,message=message)
 
+@app.route('/egreso', methods=['POST'])
+def retiro():
+    monto = int(request.form['monto'])
+    usernid = session.get('username')
+    usuario_id = ObjectId(usernid)
+    collection = db['Transaction']
+    collections = db['clientes']
+    fecha_actual = datetime.now()
+    fecha_formateada = fecha_actual.strftime("%Y-%m-%d %H:%M:%S")
+    sessions = client.start_session()
+    with sessions.start_transaction():
+        try:
+            # Obtener el monto actual del cliente
+            cliente = collections.find_one({"_id": usuario_id})
+            monto_actual = cliente['monto']
 
-@app.route('/ingreso')
+            # Verificar si el monto es suficiente para el egreso
+            if monto_actual >= monto:
+                # Realizar el egreso en una transacción
+                collection.insert_one({"client": usuario_id, "Type": 2, "mount": monto, "date": fecha_formateada})
+                # Actualizar el monto del cliente en una transacción
+                collections.update_one({"_id": usuario_id}, {"$inc": {"monto": -monto}})
+
+                sessions.commit_transaction()
+                resultado = '¡Egreso realizado correctamente!'
+            else:
+                sessions.abort_transaction()
+                resultado = 'Error: El monto actual es insuficiente para el egreso'
+
+        except:
+            sessions.abort_transaction()
+            resultado = 'Error al realizar el egreso'
+
+    sessions.end_session()
+    session['resultado_egreso'] = resultado
+    return redirect(url_for('egreso'))
+
+@app.route('/ingreso', methods=['GET'])
 def ingreso():
-    return render_template('ingreso.html')
+    usernid = session.get('username')
+    usuario_id = ObjectId(usernid)
+    collection = db['clientes']
+    print(usuario_id)
+    result = collection.find({"_id": usuario_id})
+    result=list(result)
+    for doc in result:
+        mount = doc.get("monto")
+    print(mount)
+    monto=mount
+    return render_template('ingreso.html',monto=monto)
+
+@app.route('/ingreso' ,methods=['POST'])
+def guardar():
+    monto = request.form['monto']
+    print(monto)
+    # Actualizar el monto del cliente en la base de datos
+    usernid = session.get('username')
+    print(usernid)
+    usuario_id = ObjectId(usernid)
+    collection = db['Transaction']
+    collections = db['clientes']
+    # Obtener la fecha y hora actual
+    fecha_actual = datetime.now()
+    # Formatear la fecha al formato deseado
+    fecha_formateada = fecha_actual.strftime("%Y-%m-%d %H:%M:%S")
+    sessions = client.start_session()  # Iniciar sesión de transacción
+    with sessions.start_transaction():
+        try:
+          
+  # Realizar el ingreso en una transacción
+            collection.insert_one({"client": usuario_id, "Type": 1, "mount": monto,"date":fecha_formateada})
+            # Actualizar el monto del cliente en una transacción
+            collections.update_one({"_id": usuario_id}, {"$inc": {"monto": int(monto)}})
+
+            sessions.commit_transaction()  # Confirmar la transacción
+
+            resultado = '¡Monto actualizado correctamente!'
+        except:
+            session.abort_transaction()  # Hacer rollback en caso de error
+            resultado = 'Error al actualizar el monto'
+
+    sessions.end_session()  
+    return redirect(url_for('ingreso'))
 
 @app.route('/actualizar')
 def actualizar():
@@ -57,8 +153,12 @@ def dashboard():
     print(usuario_id)
     result = collection.find({"client": usuario_id})
     result=list(result)
-    for doc in result:
-        mount = doc.get("mount")
+    print(result)
+    client = db['clientes']
+    print(usuario_id)
+    mount = client.find({"_id": usuario_id})
+    for doc in mount:
+        mount = doc.get("monto")
     print(mount)
     monto=mount
     if result:
@@ -70,8 +170,9 @@ def dashboard():
     return render_template('dashboard.html',data=result,monto=monto)
 @app.route('/logout')
 def logout():
-    session.pop('username')
-    return render_template('index.html')
+    if 'username' in session:
+        session.pop('username')
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
