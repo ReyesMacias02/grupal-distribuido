964139613035d2b6df10724830dbeaf4b93e3f8c
jose reyes <jjrm1351828189@gmail.com>
2023-06-25 04:15:56 -0500

    cambio a plantilla

--- grupal-final/app.py
+++ grupal-final/app.py
@@ -173,6 +173,10 @@ def logout():
     if 'username' in session:
         session.pop('username')
     return redirect(url_for('index'))
+@app.route('/perfil')
+def perfil():
+
+       return render_template('perfil.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
